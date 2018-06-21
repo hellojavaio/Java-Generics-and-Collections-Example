5829b27b2c18de1f0c7e1af905b9d4868a16c242
maskleo <masque@aliyun.com>
2018-06-21 19:54:24 +0800

    add Access

--- ch02/src/edu/maskleo/generics/ch02/InvokeHandler.java
+++ ch02/src/edu/maskleo/generics/ch02/InvokeHandler.java
@@ -4,17 +4,22 @@ import java.lang.reflect.Method;
 
 public class InvokeHandler {
 
-    public static void invoke(Class clazz,String methodName){
-        System.out.println(String.format("class:%s,method:%s-----begin",clazz,methodName));
+    public static void invoke(Class clazz, String methodName) {
+        System.out.println(String.format("class:%s,method:%s-----begin", clazz, methodName));
         try {
             Method method = clazz.getDeclaredMethod(methodName);
-            method.setAccessible(true);
+            boolean isAccess = method.isAccessible();
+            if (!isAccess) {
+                method.setAccessible(true);
+            }
             method.invoke(null);
-            method.setAccessible(false);
-        }catch (Exception e){
+            if (!isAccess) {
+                method.setAccessible(false);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println(String.format("class:%s,method:%s-----end",clazz,methodName));
+        System.out.println(String.format("class:%s,method:%s-----end", clazz, methodName));
     }
 
 }
