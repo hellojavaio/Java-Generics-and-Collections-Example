43fd840bd08ade954e71ca44929b47f2030610d5
maskleo <masque@aliyun.com>
2018-06-21 17:04:15 +0800

    add

--- ch02/src/edu/maskleo/generics/ch02/Test03.java
+++ ch02/src/edu/maskleo/generics/ch02/Test03.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class Test03 {
 
     public static <T> void copy(List<? super T> dst, List<? extends T> src) {
+
         for (int i = 0; i < src.size(); i++) {
             dst.set(i, src.get(i));
         }
