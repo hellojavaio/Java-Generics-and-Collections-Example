555e89bbe9c1e470c27b860fb44588c947d2d344
maskleo <masque@aliyun.com>
2018-07-06 11:43:15 +0800

    format code

--- ch02/src/edu/maskleo/generics/ch02/Test09.java
+++ ch02/src/edu/maskleo/generics/ch02/Test09.java
@@ -1,7 +1,6 @@
 package edu.maskleo.generics.ch02;
 
 public enum Test09 implements Test10 {
-
     INSTANCE {
         @Override
         public String print() {
@@ -9,18 +8,14 @@ public enum Test09 implements Test10 {
             return "INSTANCE";
         }
     };
-
-
     @Override
     public String print() {
         System.out.println(build());
         return "def";
     }
-
     interface Print {
         String print();
     }
-
     public static void main(String[] args) {
         System.out.println(INSTANCE.print());
     }
--- ch02/src/edu/maskleo/generics/ch02/Test10.java
+++ ch02/src/edu/maskleo/generics/ch02/Test10.java
@@ -1,11 +1,8 @@
 package edu.maskleo.generics.ch02;
 
 public interface Test10 {
-
     String print();
-
     default String build() {
         return "interface.Test10.default.build";
     }
-
 }
