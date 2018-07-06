767fccc174a6b97e5b8a92a0db2731bd5419827f
maskleo <masque@aliyun.com>
2018-07-06 11:31:03 +0800

    add enum about example

---
+++ ch02/src/edu/maskleo/generics/ch02/Test09.java
@@ -0,0 +1,27 @@
+package edu.maskleo.generics.ch02;
+
+public enum Test09 implements Test10 {
+
+    INSTANCE {
+        @Override
+        public String print() {
+            System.out.println(super.build());
+            return "INSTANCE";
+        }
+    };
+
+
+    @Override
+    public String print() {
+        System.out.println(build());
+        return "def";
+    }
+
+    interface Print {
+        String print();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(INSTANCE.print());
+    }
+}
---
+++ ch02/src/edu/maskleo/generics/ch02/Test10.java
@@ -0,0 +1,11 @@
+package edu.maskleo.generics.ch02;
+
+public interface Test10 {
+
+    String print();
+
+    default String build() {
+        return "interface.Test10.default.build";
+    }
+
+}
